cb3eafebbe53a2d6d5c8b535fa1505424513839f
pvdrh <duyvpvnua@gmail.com>
2021-01-05 21:57:01 +0700

    ex2

--- src/components/Unit1/ex1.vue
+++ src/components/Unit1/ex1.vue
@@ -1,26 +1,87 @@
 <template>
-    <div class="container">
-        <div>
-            <p>Họ và tên</p>
-            <input type="text" id='name' value=''> 
-        </div>
-        <div>
-            <p>Ngày sinh</p>
-            <input type="date" id='date' value=''> 
-        </div>
-        <div>
-            <p>Số điện thoại</p>
-            <input type="text" id='phone' value=''> 
-        </div>
-         <div>
-            <p>Link facebook</p>
-            <input type="text" id='fb' value=''> 
-        </div>
-        <button id='submit-btn'>Go</button>
-</div>
+  <div class="container">
+    <div class="loginWrap">
+      <div class="inputWrap">
+        <div class="inputLabel">Nhập tiêu đề</div>
+        <input type="text" v-model="title"/>
+      </div>
+      <div class="inputWrap">
+        <div class="inputLabel">Nhập nội dung</div>
+        <textarea name="" id="" cols="30" rows="10" v-model="strHtml"></textarea>
+      </div>
+    </div>
+    <div class="infoWrap">
+      <div><h2>{{title}}</h2></div>
+      <div v-html="strHtml"></div>
+
+    </div>
+  </div>
 </template>
 
-<style>
+<script>
+export default {
+    name:"Lesson1",
+    data () {
+      return {
+        title:"",
+        strHtml:""
+      }
+    }
     
+}
+</script>
 
+<style lang="scss">
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  .loginWrap {
+    background: #f5f5f5;
+    border: 1px solid #6e6e6e;
+    border-radius: 5px;
+    padding: 24px;
+    height: 250px;
+    width: 300px;
+    .inputWrap {
+      margin-bottom: 12px;
+      
+      .inputLabel {
+        font-weight: bold;
+        margin-bottom: 8px;
+        text-align: left;
+      }
+      textarea {
+        width: 292px;
+        resize: none;
+      }
+      input {
+        width: 292px;
+        height: 30px;
+      }
+    }
+    button {
+      width: 100%;
+      height: 40px;
+      border-radius: 5px;
+      margin-top: 20px;
+      background: #0080dd;
+      color: #fff;
+      border: unset;
+      font-weight: bold;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
+  .infoWrap {
+    background: #fff;
+    border: 1px solid #6e6e6e;
+    border-radius: 5px;
+    padding: 24px;
+    height: 250px;
+    width: 250px;
+    margin-left: 24px;
+  }
+}
 </style>
