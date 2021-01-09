0db994cdab3664a66a6aee9fecc10a1d30cf1cb4
pvdrh <duyvpvnua@gmail.com>
2021-01-09 18:53:41 +0700

    fix folder

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 // import ex1 from './components/Unit1/ex1.vue'
-import Lession2 from './components/Lession2.vue'
+import Lession2 from './components/Unit2/Lession2.vue'
 
 export default {
   name: 'App',
