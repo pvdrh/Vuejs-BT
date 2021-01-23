5e5bc783a277124dc615d078c703b80dd99aeba7
pvdrh <duyvpvnua@gmail.com>
2021-01-23 09:53:02 +0700

    fix background ex

--- src/components/Unit5/LoginForm.vue
+++ src/components/Unit5/LoginForm.vue
@@ -1,69 +1,85 @@
 <template>
-<div class="container">
-  <div class="Wrap">
-    <div class="logo">
-      <img src="http://dev-fms.zentsoft.com/static/media/logo-login.2d516aef.png" alt="">
+  <div class="container">
+    <div class="Wrap">
+      <div class="logo">
+        <img
+          src="http://dev-fms.zentsoft.com/static/media/logo-login.2d516aef.png"
+          alt=""
+        />
+      </div>
+      <div class="inputWrap" style="margin-bottom: 24px">
+        <el-input
+          placeholder="Email"
+          v-model="username"
+          type="text"
+          @input="onChangeUser($event)"
+        ></el-input>
+        <div v-if="errorUsername" class="error">
+          <i class="el-icon-warning"></i> Email không được bỏ trống
+        </div>
+      </div>
+      <div class="inputWrap">
+        <el-input
+          placeholder="Mật khẩu"
+          v-model="password"
+          type="password"
+          @input="onChangePass($event)"
+        ></el-input>
+        <div v-if="errorPass" class="error">
+          <i class="el-icon-warning"></i> Mật khẩu không được bỏ trống
+        </div>
+      </div>
+      <div class="forgotPass">
+        <el-button @click="forgotPass()">Quên mật khẩu?</el-button>
+      </div>
+      <button class="btn-login" @click="login()">ĐĂNG NHẬP</button>
     </div>
-    <div class="inputWrap" style="margin-bottom: 24px">
-      <el-input placeholder="Email" v-model="username" type="text" @input="onChangeUser($event)"></el-input>
-      <div v-if="errorUsername" class="error"><i class="el-icon-warning"></i> Email không được bỏ trống</div>
-    </div>
-    <div class="inputWrap">
-      <el-input placeholder="Mật khẩu" v-model="password" type="password" @input="onChangePass($event)"></el-input>
-      <div v-if="errorPass" class="error"><i class="el-icon-warning"></i> Mật khẩu không được bỏ trống</div>
-    </div>
-    <div class="forgotPass">
-      <el-button @click="forgotPass()">Quên mật khẩu?</el-button>
-    </div>
-    <button class="btn-login" @click="login()">
-      ĐĂNG NHẬP
-    </button>
   </div>
-</div>
 </template>
 
 <script>
-  export default {
+export default {
   name: "LoginForm",
-    data() {
-      return {
-        username: '',
-        password: '',
-        errorUsername: false,
-        errorPass: false,
+  data() {
+    return {
+      username: "",
+      password: "",
+      errorUsername: false,
+      errorPass: false,
+    };
+  },
+  methods: {
+    forgotPass() {
+      this.$router.push("forgot-password");
+    },
+    login() {
+      if (this.username.length == "") {
+        this.errorUsername = true;
+      }
+      if (this.password.length == "") {
+        this.errorPass = true;
       }
     },
-    methods: {
-      forgotPass() {
-        this.$router.push('forgot-password')
-      },
-      login() {
-        if (this.username.length == '') {
-          this.errorUsername = true
-        }
-        if (this.password.length == '') {
-          this.errorPass = true
-        }
-      },
-      onChangeUser(user) {
-        this.username = user
-        this.errorUsername = false
-      },
-      onChangePass(pass) {
-        this.password = pass
-        this.errorPass = false
-      }
-    }
-  }
+    onChangeUser(user) {
+      this.username = user;
+      this.errorUsername = false;
+    },
+    onChangePass(pass) {
+      this.password = pass;
+      this.errorPass = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .container{
+.container {
   background-color: #0093e9;
-    .Wrap {
+  background-image: linear-gradient(160deg, #0093e9, #80d0c7);
+  .Wrap {
     width: 444px;
     padding: 24px;
-    background-color:white;
+    background-color: white;
     border-radius: 10px;
     box-sizing: border-box;
     .logo {
@@ -107,5 +123,5 @@
       margin-top: 24px;
     }
   }
-  }
+}
 </style>
